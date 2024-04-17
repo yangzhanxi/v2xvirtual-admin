7d554ad6271da7ce809edeb5738533e4ecd1ab41
yangzhanxi <yangzhanxi@163.com>
2024-04-17 10:02:53 +0800

    Increased token expriation

--- app/services/network_management/network_blueprint.py
+++ app/services/network_management/network_blueprint.py
@@ -16,14 +16,13 @@ net = Blueprint("network", __name__)
 @net.get("/ports")
 @jwt_required()
 @roles_accepted(ADMIN_ROLE)
-def get_licenses():
+def get_ports():
     """
     GET ports view.
     """
     session.permanent = True
 
     return ports_handler()
-    # return mock_ports()
 
 
 @net.get("/partnum")
@@ -36,111 +35,3 @@ def get_part_num():
     session.permanent = True
 
     return part_num_handler()
-    # return mock_part_num()
-
-
-def mock_ports():
-    ret = [
-        {
-            "address": {
-                "label": "IP Address", "value": "172.16.20.22"},
-            "auto_negotiation": {
-                "label": "Auto-Negotiation", "value": "true"},
-            "auto_negotiation_role": {
-                "label": "Auto-Negotiation Role", "value": "MASTER"},
-            "duplex_mode": {
-                "label": "Duplex Mode", "value": "FULL"},
-            "gateway": {
-                "label": "Gateway", "value": ""},
-            "ip_version": {
-                "label": "IP version", "value": "IPv4"},
-            "link_status": {
-                "label": "Link Status", "value": "UP"},
-            "line_speed": {
-                "label": "Line Speed", "value": "Speed"},
-            "netmask": {
-                "label": "Netmask", "value": "255.255.255.0"},
-            "port_handel": {
-                "label": "Port Handel", "value": "port1"},
-            "port_name": {
-                "label": "Port Name", "value": "slot1port1"}
-        },
-        {
-            "address": {
-                "label": "IP Address", "value": "172.16.21.22"},
-            "auto_negotiation": {
-                "label": "Auto-Negotiation", "value": "true"},
-            "auto_negotiation_role": {
-                "label": "Auto-Negotiation Role", "value": "MASTER"},
-            "duplex_mode": {
-                "label": "Duplex Mode", "value": "FULL"},
-            "gateway": {
-                "label": "Gateway", "value": ""},
-            "ip_version": {
-                "label": "IP version", "value": "IPv4"},
-            "link_status": {
-                "label": "Link Status", "value": "DOWN"},
-            "line_speed": {
-                "label": "Line Speed", "value": "Speed"},
-            "netmask": {
-                "label": "Netmask", "value": "255.255.255.0"},
-            "port_handel": {
-                "label": "Port Handel", "value": "port1"},
-            "port_name": {
-                "label": "Port Name", "value": "slot1port2"}
-        },
-        {
-            "address": {
-                "label": "IP Address", "value": "172.16.22.22"},
-            "auto_negotiation": {
-                "label": "Auto-Negotiation", "value": "true"},
-            "auto_negotiation_role": {
-                "label": "Auto-Negotiation Role", "value": "MASTER"},
-            "duplex_mode": {
-                "label": "Duplex Mode", "value": "FULL"},
-            "gateway": {
-                "label": "Gateway", "value": ""},
-            "ip_version": {
-                "label": "IP version", "value": "IPv4"},
-            "link_status": {
-                "label": "Link Status", "value": "DOWN"},
-            "line_speed": {
-                "label": "Line Speed", "value": "Speed"},
-            "netmask": {
-                "label": "Netmask", "value": "255.255.255.0"},
-            "port_handel": {
-                "label": "Port Handel", "value": "port1"},
-            "port_name": {
-                "label": "Port Name", "value": "slot1port3"}
-        },
-        {
-            "address": {
-                "label": "IP Address", "value": "172.16.23.22"},
-            "auto_negotiation": {
-                "label": "Auto-Negotiation", "value": "true"},
-            "auto_negotiation_role": {
-                "label": "Auto-Negotiation Role", "value": "MASTER"},
-            "duplex_mode": {
-                "label": "Duplex Mode", "value": "FULL"},
-            "gateway": {
-                "label": "Gateway", "value": ""},
-            "ip_version": {
-                "label": "IP version", "value": "IPv4"},
-            "link_status": {
-                "label": "Link Status", "value": "ERROR"},
-            "line_speed": {
-                "label": "Line Speed", "value": "Speed"},
-            "netmask": {
-                "label": "Netmask", "value": "255.255.255.0"},
-            "port_handel": {
-                "label": "Port Handel", "value": "port1"},
-            "port_name": {
-                "label": "Port Name", "value": "slot1port4"}
-        }
-    ]
-
-    return ret
-
-
-def mock_part_num():
-    return {"part_num": "SPT-C50"}
